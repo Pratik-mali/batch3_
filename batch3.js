1b07e5e6ee130c6a30fb7d1e57957b2d3cab0a07
Pratik-mali <pratikm057@gmail.com>
2024-11-12 21:55:11 +0530

    Prati

---
+++ batch3.js
@@ -0,0 +1,28 @@
+let a = [1,4,8,10,12,15,16,20]  //5 7  
+let num = 16
+
+// for(let i = 0; i<= a.length;i++){
+//     if(num == a[i]){
+//         console.log(i)
+//     }
+// }
+
+let start = 0
+let end = a.length
+
+
+while(start <= end){
+let mid = parseInt((start+end)/2)   //12/2=  6
+
+    if(a[mid] == num){
+        console.log(mid)
+        break;
+    }
+    else if(num < a[mid]){
+        end = mid - 1 
+    }
+
+    else if(num > a[mid]){
+        start = mid  + 1
+    }
+}
